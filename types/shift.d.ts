dfe2921fef50a85485829a95e6e5d612850a9a37
Georg Oldenburger <georgoldenb@gmail.com>
2023-01-21 10:25:35 +0100

    Version 1.0.0 Beta6

--- types/shift.d.ts
+++ types/shift.d.ts
@@ -1,3 +1,5 @@
 export { partial } from "./basic/partial.js";
 export { curry } from "./basic/curry.js";
+export { compose } from "./basic/compose.js";
+export { pipe } from "./basic/pipe.js";
 export { PersistentVector } from "./persistent/vector.js";
