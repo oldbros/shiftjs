90f98c54fb64be823a717d9553617da881a31dcc
Georg Oldenburger <georgoldenb@gmail.com>
2023-02-01 21:36:56 +0100

    implement more partial functions

--- src/shift.js
+++ src/shift.js
@@ -1,4 +1,12 @@
-export { partial } from './basic/partial.js';
+export {
+  partial,
+  partialOne,
+  partialReverse,
+  partialReverseOne,
+  partialObject,
+  partialObjectLast,
+  partialObjectFirst,
+} from './basic/partial.js';
 export { curry } from './basic/curry.js';
 export { compose } from './basic/compose.js';
 export { pipe } from './basic/pipe.js';
--- types/shift.d.ts
+++ types/shift.d.ts
@@ -1,5 +1,5 @@
-export { partial } from "./basic/partial.js";
 export { curry } from "./basic/curry.js";
 export { compose } from "./basic/compose.js";
 export { pipe } from "./basic/pipe.js";
 export { PersistentVector } from "./persistent/vector.js";
+export { partial, partialOne, partialReverse, partialReverseOne, partialObject, partialObjectLast, partialObjectFirst } from "./basic/partial.js";
