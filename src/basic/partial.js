61fe95a30428c9a384c197f249e4fe59ef351c24
Georg Oldenburger <georgoldenb@gmail.com>
2023-02-01 18:39:27 +0100

    partialObject implementation

--- src/basic/partial.js
+++ src/basic/partial.js
@@ -1,10 +1,16 @@
 /** @typedef {import('./.types').AnyFunc} AnyFunc*/
 
 /**
- * Partial - high performance one argument partial application function
- * multiple arguments significantly reduces performance
+ * partial - high performance multiple arguments partial application function
  * see https://en.wikipedia.org/wiki/Partial_application
- * No arguments length check
+ * Arguments length changes
  * @type {(fn: AnyFunc, ...args: any[]) => AnyFunc}
  * */
 export const partial = (fn, ...args) => fn.bind(null, ...args);
+
+/**
+ * partialObject - high performance object as argument partial application function
+ * Arguments length always 1
+ * @type {(fn: AnyFunc, args: object) => AnyFunc}
+ * */
+export const partialObject = (fn, args) => (delta) => fn(Object.assign(args, delta));
