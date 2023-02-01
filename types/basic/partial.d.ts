558595a98e1ce215c56cfbe09081cd09322a4086
Georg Oldenburger <georgoldenb@gmail.com>
2023-02-01 18:39:27 +0100

    partialObject implementation

--- types/basic/partial.d.ts
+++ types/basic/partial.d.ts
@@ -1,10 +1,15 @@
 /** @typedef {import('./.types').AnyFunc} AnyFunc*/
 /**
- * Partial - high performance one argument partial application function
- * multiple arguments significantly reduces performance
+ * partial - high performance multiple arguments partial application function
  * see https://en.wikipedia.org/wiki/Partial_application
- * No arguments length check
+ * Arguments length changes
  * @type {(fn: AnyFunc, ...args: any[]) => AnyFunc}
  * */
 export const partial: (fn: AnyFunc, ...args: any[]) => AnyFunc;
+/**
+ * partialObject - high performance object as argument partial application function
+ * Arguments length always 1
+ * @type {(fn: AnyFunc, args: object) => AnyFunc}
+ * */
+export const partialObject: (fn: AnyFunc, args: object) => AnyFunc;
 export type AnyFunc = import('./.types').AnyFunc;
